485800a06253851ae6b8a072476ab02509ba18a4
isanmh <1157050074@student.uinsgd.ac.id>
2026-08-12 09:54:25 +0700

    add: slicing ui to code

---
+++ lib/slicing/chatty/theme_chatty.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+// warna utama pada app
+// penulisan pada flutter warna hex (0xff[RRGGBB]) di dpn warna
+Color blueColor = Color(0xff1F8DF5);
+Color whiteColor = Color(0xffffffff);
+Color lightBlueColor = Color(0xff9ED0FF);
+Color blackColor = Color(0xff2C3A59);
+Color greyColor = Color(0xff808BA2);
+Color greenColor = Color(0xff29CB9E);
