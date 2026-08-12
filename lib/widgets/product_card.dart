ba36440e9320fd49093c2f5de21a98a2908547fa
isanmh <1157050074@student.uinsgd.ac.id>
2026-08-12 15:24:35 +0700

    add: style shoes

--- lib/widgets/product_card.dart
+++ lib/widgets/product_card.dart
@@ -1,10 +1,63 @@
 import 'package:flutter/material.dart';
+import 'package:my_app/slicing/shoes/theme_shoes.dart';
 
 class ProductCard extends StatelessWidget {
   const ProductCard({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      width: 215,
+      height: 278,
+      margin: EdgeInsets.only(right: defaultMargin),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        color: primaryTextColor,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Image.asset(
+            "assets/images/shoes1.png",
+            fit: BoxFit.cover,
+            width: 215,
+            height: 120,
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(height: 20),
+                Text(
+                  "TERREX URBAN LOW",
+                  style: blackTextStyle.copyWith(
+                    fontSize: 18,
+                    fontWeight: semiBold,
+                  ),
+                  maxLines: 1,
+                ),
+                SizedBox(height: 6),
+                Text(
+                  "Hiking",
+                  style: secondaryTextStyle.copyWith(
+                    fontSize: 12,
+                    fontWeight: light,
+                  ),
+                ),
+                SizedBox(height: 6),
+                Text(
+                  "\$143,98",
+                  style: priceTextStyle.copyWith(
+                    fontSize: 14,
+                    fontWeight: semiBold,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
