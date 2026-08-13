46b53fd677fadc9f69ae724af0435a8db6a6186e
isanmh <1157050074@student.uinsgd.ac.id>
2026-08-13 14:31:35 +0700

    add: notes api

--- lib/utils/contanst.dart
+++ lib/utils/contanst.dart
@@ -1,4 +1,11 @@
+import 'package:flutter/material.dart';
+
 const reqURl = 'https://reqres.in/api/users';
 const groupUrl = 'https://dummyjson.com/products';
 const albumUrl = 'https://jsonplaceholder.typicode.com/albums/2';
 const notesUrl = 'https://6a7d6805f8b2ed99ca4e3d9a.mockapi.io/api/notes';
+
+// widget constants
+showSnackBar(BuildContext context, String message) {
+  ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(message)));
+}
