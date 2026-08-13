d2bad2c217b426fd4ee8d43ea810a3967d91ac89
isanmh <1157050074@student.uinsgd.ac.id>
2026-08-13 15:26:04 +0700

    add: notes api

--- lib/navbar/google_navbar.dart
+++ lib/navbar/google_navbar.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
+import 'package:my_app/dasar/root_page.dart';
 import 'package:my_app/slicing/shoes/shoes_page.dart';
 import 'package:my_app/views/album_page.dart';
 import 'package:my_app/views/chatty_api.dart';
@@ -14,7 +15,7 @@ class GoogleNavbar extends StatefulWidget {
 
 class _GoogleNavbarState extends State<GoogleNavbar> {
   int currentPage = 0;
-  List<Widget> pages = [NotesPage(), ChattyApi(), ShoesPage(), AlbumPage()];
+  List<Widget> pages = [NotesPage(), ChattyApi(), ShoesPage(), RootPage()];
 
   @override
   Widget build(BuildContext context) {
@@ -32,15 +33,21 @@ class _GoogleNavbarState extends State<GoogleNavbar> {
           child: Padding(
             padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 8),
             child: GNav(
+              backgroundColor: Colors.white,
               rippleColor: Colors.grey[300]!,
               hoverColor: Colors.grey[100]!,
               gap: 8,
-              activeColor: Colors.black,
+              activeColor: Colors.white,
               iconSize: 24,
               padding: EdgeInsets.symmetric(horizontal: 20, vertical: 12),
               duration: Duration(milliseconds: 400),
-              tabBackgroundColor: Colors.grey[100]!,
-              color: Colors.black,
+              tabBackgroundColor: Colors.purple,
+              tabBackgroundGradient: LinearGradient(
+                colors: [Colors.deepPurple, Colors.purpleAccent],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
+              color: Colors.purple,
               tabs: [
                 GButton(icon: Icons.home, text: 'Home'),
                 GButton(icon: Icons.favorite, text: 'Likes'),
